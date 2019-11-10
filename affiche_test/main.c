4a508276cbbb41a667fe58b443777dcb2ffaa7cf
BASSINAS <edembassine@gmail.com>
2019-11-10 13:46:37 +0100

    ajoute de la fonction la_somme(int a,int b) le programme calcul a present la somme

--- affiche_test/main.c
+++ affiche_test/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int la_somme(int a, int b);
 int main()
 {
+    int nombre_1,nombre_2;
     printf(" BONJOUR LES ZOZOS COMMENT VOUS ALLEZ!\n");
+    printf("entrer le premier nombre : ");
+    scanf("%d",&nombre_1);
+    printf("entrer le deuxieme nombre : ");
+    scanf("%d",&nombre_2);
+    printf(" la somme est : %d",la_somme(nombre_1,nombre_2));
     return 0;
 }
+int la_somme(int a, int b)
+    {
+        return a+b;
+    }
